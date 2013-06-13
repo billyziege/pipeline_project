e2bc6bcbd54ad89db30e6286d24369573a31f3ed
Brandon Zerbe <zerbeb@humgen.ucsf.edu>
2013-06-13 11:17:47 -0700

    A couple more edits to the new physical_objects models.

--- pipeline/physical_objects/models.py
+++ pipeline/physical_objects/models.py
@@ -3,9 +3,18 @@ from mockdb.models import KeyedObject
 # Create your models here.
 
 class Sample(KeyedObject):
-    def __init__(self,config,key='dummy',state='active',project=None):
+    """
+    The basic unit of sample.
+    """
+
+    def __init__(self,config,key='dummy',state='active',project=None,plate=None,partner_key=None):
+        """
+        Initialize the sample object.
+        """
         self.state = state
         self.project = project
+        self.current_plate = plate
+        self.partner_key = partner_key #The active partnering.
         try:
             int(key[0])
             self.key = "Sample_" + str(key)
@@ -14,17 +23,40 @@ class Sample(KeyedObject):
         self.obj_type = self.__class__.__name__
 
     def change_state(self,state):
+        """
+        Offers a hook for changing the state.
+        """
         self.state = state
 
+    def __is_matched__(self):
+        """
+        Returns true if the sample is paired with another sample.
+        """
+        if self.partner_key is None:
+            return False
+        return True
+
 class Case(Sample):
+    """
+    The sub-class of sample.
+    """
     pass
 
 class Control(Sample):
+    """
+    The sub-class of sample.
+    """
     pass
 
 class CaseControlPair(KeyedObject):
+    """
+    Contains the relational information of a possible case control pair.
+    """
 
     def __init__(self,config,case=None,control=None,*args,**kwargs):
+        """
+        Initializes the case control pair relation object.
+        """
         if case is None:
             case = Case(config,key='dummy_case_key')
         if control is None:
@@ -37,7 +69,3 @@ class CaseControlPair(KeyedObject):
         self.obj_type = self.__class__.__name__
         self.case_key = case.key
         self.control_key = control.key
-    def __str__(self):
-        return self.key
-    def __unicode__(self):
-        return self.key
